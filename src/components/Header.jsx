a58668ab9811f76e79c6a3f885e5129213b149ca
Vyacheslav <vyacheslav.sokolovv@icloud.com>
2021-03-07 20:42:42 +0300

    start project
    create first vision this project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src="http://www.pngall.com/wp-content/uploads/2016/05/WordPress-Logo-Free-Download-PNG.png" alt="Wikipedia"/>
+    </header>
+    );
+}
+
+export default Header;
