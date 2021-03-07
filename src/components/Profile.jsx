4d96ddd87889bc53d2f0cc5ad420ad0e974af00b
Vyacheslav <vyacheslav.sokolovv@icloud.com>
2021-03-07 20:57:27 +0300

    css-modules
    add css-modules

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src="https://www.goodfreephotos.com/albums/canada/alberta/banff-national-park/very-majestic-and-beautiful-landscape-with-mountains-in-banff-national-park-alberta-canada.jpg" alt="Picture"/></div>
+            <div>avatar + description</div>
+            <div>My posts
+                <div>New post</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
